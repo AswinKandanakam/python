9d196bb6080619df01bc5e7c983e97822226b477
aswin kandanakam <aswinvkandnakam@gmail.com>
2022-06-11 11:28:22 +0530

    largest

--- largest.py
+++ largest.py
@@ -1,8 +1,8 @@
 a=int(input('enter first number'))
 b=int(input('enter second number'))
 c=int(input('enter third number'))
-if a>c:
-    print(a,'is greater')
+if a>b and a>c:
+        print(a,'is greater')
 elif b>c:
         print(b,'is greater')
 else:
